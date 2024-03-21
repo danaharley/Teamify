487182f732afe41d38678f90c42adad254f8aff3
danaharley <danaharley.github@gmail.com>
2024-03-21 21:43:39 +0700

    auth setup

---
+++ app/(platform)/protected/page.tsx
@@ -0,0 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+
+const ProtectedPage = () => {
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+};
+
+export default ProtectedPage;

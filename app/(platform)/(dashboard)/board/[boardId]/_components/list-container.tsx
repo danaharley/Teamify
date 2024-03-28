c73f70df7c743118e8c463d7560bfcd8ea3ad14b
danaharley <danaharley.github@gmail.com>
2024-03-28 20:16:28 +0700

    list header: update list title

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -1,7 +1,11 @@
 "use client";
 
-import { ListWithCards } from "@/types";
+import { useEffect, useState } from "react";
+
 import { ListForm } from "./list-form";
+import { ListItem } from "./list-item";
+
+import { ListWithCards } from "@/types";
 
 type ListContainerProps = {
   boardId: string;
@@ -9,8 +13,17 @@ type ListContainerProps = {
 };
 
 export const ListContainer = ({ boardId, data }: ListContainerProps) => {
+  const [orderedData, setOrderedData] = useState(data);
+
+  useEffect(() => {
+    setOrderedData(data);
+  }, [data]);
+
   return (
-    <ol>
+    <ol className="flex h-full gap-x-3">
+      {orderedData.map((list, index) => (
+        <ListItem key={list.id} index={index} data={list} />
+      ))}
       <ListForm />
       <div className="w-1 flex-shrink-0" />
     </ol>
---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { ElementRef, useRef, useState } from "react";
+
+import { ListHeader } from "./list-header";
+
+import { ListWithCards } from "@/types";
+
+type ListItemProps = {
+  data: ListWithCards;
+  index: number;
+};
+
+export const ListItem = ({ data, index }: ListItemProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const textareaRef = useRef<ElementRef<"textarea">>(null);
+
+  const disableEditing = () => {
+    setIsEditing(false);
+  };
+
+  const enableEditing = () => {
+    setIsEditing(true);
+    setTimeout(() => {
+      textareaRef.current?.focus();
+    });
+  };
+  return (
+    <li className="h-full w-[272px] shrink-0 select-none">
+      <div className="w-full rounded-md bg-[#1f1f2f4] pb-2 shadow-md">
+        <ListHeader data={data} onAddCard={enableEditing} />
+      </div>
+    </li>
+  );
+};

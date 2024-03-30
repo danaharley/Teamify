b419296e5621a3a3fbfe42f084f3034914175144
danaharley <danaharley.github@gmail.com>
2024-03-31 01:20:37 +0700

    card modal

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -3,12 +3,16 @@
 import { Draggable } from "@hello-pangea/dnd";
 import { Card } from "@prisma/client";
 
+import { useCardModal } from "@/hooks/use-card-modal";
+
 type CardItemProps = {
   index: number;
   data: Card;
 };
 
 export const CardItem = ({ data, index }: CardItemProps) => {
+  const cardModal = useCardModal();
+
   return (
     <Draggable draggableId={data.id} index={index}>
       {(provided) => (
@@ -17,6 +21,7 @@ export const CardItem = ({ data, index }: CardItemProps) => {
           {...provided.draggableProps}
           {...provided.dragHandleProps}
           ref={provided.innerRef}
+          onClick={() => cardModal.onOpen(data.id)}
           className="truncate rounded-md border-2 border-transparent px-3 py-2 text-sm shadow-sm hover:border-black"
         >
           {data.title}
